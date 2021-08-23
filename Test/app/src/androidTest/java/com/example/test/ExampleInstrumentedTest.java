8eb8d5774c88d0c3f78cd3531d7d0aac69946c37
Hyunji <zlxl0108@naver.com>
2021-08-23 23:24:50 +0900

    test

--- Test/app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
+++ Test/app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
@@ -22,7 +22,5 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.test", appContext.getPackageName());
-
-
     }
 }
